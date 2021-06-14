f8f495ba7e4c557d00e0a3fd251fe8b7fc284619
David Kumar <david.dn.kumar@gmail.com>
2021-06-14 17:12:40 -0500

    add dependencies

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import app from './app';
+import App from './app';
 
-ReactDOM.render(<app />, 'root');
+ReactDOM.render(<App />, document.getElementById('root'));
 
