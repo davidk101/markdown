d6192d2cc416345760bc6fff959798294bb9a4c5
David Kumar <david.dn.kumar@gmail.com>
2021-06-15 16:06:04 -0500

    add styled components styling for landing page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
+import { GlobalStyles } from './global-styles'; // add fragment since jsx elements adjacent must be enclosed
+import 'normalize.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<>
+    <GlobalStyles />
+    <App />
+    < />,
+    document.getElementById('root'));
 
